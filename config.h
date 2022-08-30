3c1470e970135911c69015e03336d4bc80ca7c34
asifZaman0362 <zero362001@protonmail.com>
2022-08-30 23:32:09 +0530

    Added patches

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
-static int borderpx = 2;
+static char *font = "Fantasque Sans Mono:pixelsize=20:antialias=true:autohint=true";
+static int borderpx = 10;
 
 /*
  * What program is execed by st depends of these precedence rules:
@@ -164,7 +164,7 @@ static const ColorScheme schemes[] = {
 };
 
 static const char * const * colorname;
-int colorscheme = 0;
+int colorscheme = 6;
 
 /*
  * Default colors (colorname index)
